d4870cc157cbc113783f1bbece2e872f28316f21
Jorge Navarro Gonzalez <jorge.navarro@dots-robotics.com>
2022-01-14 00:52:47 +0100

    moving SideMenu and AppBar to dashboard screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,17 +1,12 @@
 import 'package:cleanin/screens/dashboard_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:cleanin/screens/side_menu.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Hello cleanin!"),
-      ),
-      drawer: SideMenu(),
+    return const Scaffold(
       body: SafeArea(
         child: DashboardScreen(),
       ),
