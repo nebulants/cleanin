29449416b32311cd41330734c479abe15b4417fb
Jorge Navarro Gonzalez <jorge.navarro@dots-robotics.com>
2022-01-14 00:47:11 +0100

    Event calendar to display current entries from imported ical calendar. It will not import the calendar but get it from ical and convert it to a calendar entry

---
+++ lib/screens/calendar/event_calendar.dart
@@ -0,0 +1,158 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:cleanin/utils/calendar_parser.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cleanin/models/home.dart';
+
+class CalendarScreen extends StatefulWidget {
+  /// Creates the home page to display the calendar widget.
+  const CalendarScreen({Key? key, required this.reference}) : super(key: key);
+
+  final DocumentReference<Home> reference;
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  List<Meeting> _dataSource = <Meeting>[];
+  late CalendarParser _calendarParser;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarParser = CalendarParser();
+    getCalendarEntries().then((value) => setState(() {
+      _dataSource = value;
+    }));
+  }
+
+  Future<List<Meeting>> getCalendarEntries() async {
+
+    Home home = await widget.reference.get().then((value) => value.data()!);
+
+    List<Meeting> meetings = await _calendarParser.getCalendarEntries(home.icalPermalink);
+
+    meetings.sort((a, b) => a.from.compareTo(b.from));
+
+    var filtered = meetings.where((e) => e.from.isAfter(DateTime.now()));
+
+    print(filtered.map((e) => e.from));
+
+    home.nextCheckIn = filtered.first.from;
+
+    await widget.reference.update(home.toMap());
+
+    return meetings;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Event Calendar"),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                List<Meeting> tempMeetings = await getCalendarEntries();
+                setState(() {
+                  _dataSource = tempMeetings;
+                });
+              },
+              icon: const Icon(Icons.sync),
+            )
+          ],
+        ),
+        body: SfCalendar(
+          view: CalendarView.month,
+          dataSource: MeetingDataSource(_dataSource),
+          // by default the month appointment display mode set as Indicator, we can
+          // change the display mode as appointment using the appointment display
+          // mode property
+          monthViewSettings: const MonthViewSettings(
+            appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+            numberOfWeeksInView: 4
+          ),
+          allowedViews: const [
+            CalendarView.day,
+            CalendarView.week,
+            CalendarView.month
+          ],
+          firstDayOfWeek: DateTime.monday,
+          showDatePickerButton: true,
+        )
+        // body: CalendarAppointmentEditor(UniqueKey())
+    );
+  }
+}
+
+/// An object to set the appointment collection data source to calendar, which
+/// used to map the custom appointment data to the calendar appointment, and
+/// allows to add, remove or reset the appointment collection.
+class MeetingDataSource extends CalendarDataSource {
+  /// Creates a meeting data source, which used to set the appointment
+  /// collection to the calendar
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return _getMeetingData(index).from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return _getMeetingData(index).to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return _getMeetingData(index).eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return _getMeetingData(index).background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return _getMeetingData(index).isAllDay;
+  }
+
+  Meeting _getMeetingData(int index) {
+    final dynamic meeting = appointments![index];
+    late final Meeting meetingData;
+    if (meeting is Meeting) {
+      meetingData = meeting;
+    }
+
+    return meetingData;
+  }
+}
+
+/// Custom business object class which contains properties to hold the detailed
+/// information about the event data which will be rendered in calendar.
+class Meeting {
+  /// Creates a meeting class with required details.
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  /// Event name which is equivalent to subject property of [Appointment].
+  String eventName;
+
+  /// From which is equivalent to start time property of [Appointment].
+  DateTime from;
+
+  /// To which is equivalent to end time property of [Appointment].
+  DateTime to;
+
+  /// Background which is equivalent to color property of [Appointment].
+  Color background;
+
+  /// IsAllDay which is equivalent to isAllDay property of [Appointment].
+  bool isAllDay;
+}
