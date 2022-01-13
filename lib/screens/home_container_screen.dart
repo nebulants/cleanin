21cad08528a0ca888747e821eae5cf9e39e3065d
Jorge Navarro Gonzalez <jorge.navarro@dots-robotics.com>
2022-01-14 00:53:39 +0100

    inject DocumentReference instead of just uid

--- lib/screens/home_container_screen.dart
+++ lib/screens/home_container_screen.dart
@@ -1,41 +1,49 @@
-import 'package:cleanin/screens/components/table_calendar_basic.dart';
 import 'package:flutter/material.dart';
-import 'package:cleanin/screens/login_screen.dart';
+import 'package:cleanin/screens/home_status_screen.dart';
+import 'package:cleanin/screens/calendar/event_calendar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cleanin/models/home.dart';
 
 class HomeContainerScreen extends StatefulWidget {
-  const HomeContainerScreen({Key? key}) : super(key: key);
+  const HomeContainerScreen({Key? key, required this.reference}) : super(key: key);
+
+  final DocumentReference<Home> reference;
 
   @override
-  State createState() => _HomeContainerScreen();
+  State createState() => _HomeContainerScreenState();
 }
 
-class _HomeContainerScreen extends State<HomeContainerScreen> {
-  final List<Widget> _homeContainerViews = [
-    const LoginScreen(),
-    const TableBasicsExample(),
-  ];
+class _HomeContainerScreenState extends State<HomeContainerScreen> {
+  late final List<Widget> _homeContainerViews;
   int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _homeContainerViews = [
+      CalendarScreen(reference: widget.reference),
+      HomeStatusScreen(reference: widget.reference),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _homeContainerViews[_currentIndex],
-      appBar: AppBar(
-          title: const Text("Home container screen")
-      ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
         onTap: (index) => setState(() {
           _currentIndex = index;
         }),
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: "calendar"
+          ),
+          BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: "home"
           ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today),
-              label: "calendar"
-          )
         ],
       ),
     );
